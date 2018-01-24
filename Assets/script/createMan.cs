cf7555382cd27f9756ae89cf34e29052fb4acf08
Wan-Ting Huang <103703004@nccu.edu.tw>
2018-01-24 15:31:41 +0800

    init

---
+++ Assets/script/createMan.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class createMan : MonoBehaviour {
+
+    public GameObject man1;
+    public GameObject man2;
+    public GameObject man3;
+    public GameObject man4;
+    public GameObject man5;
+    private GameObject body;
+    private GameObject m;
+    //private string url = "D://政大/數位內容畢製/face/Assets/img/body.png";
+    private string url = "https://firebasestorage.googleapis.com/v0/b/webfinal-e58b9.appspot.com/o/%E5%83%91%E7%B7%A3.jpg?alt=media&token=f7634a76-4b94-4ac1-b57c-80f5eaaa0e4f";
+    private int manNumber;
+
+    // Use this for initialization
+    void Start () {
+        manNumber = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(manNumber == 1)
+                m = Instantiate(man1, new Vector3(0, 0, 90), Quaternion.identity);
+            else if (manNumber == 2)
+                m = Instantiate(man2, new Vector3(0, 0, 90), Quaternion.identity);
+            else if (manNumber == 3)
+                m = Instantiate(man3, new Vector3(0, 0, 90), Quaternion.identity);
+            else if (manNumber == 4)
+                m = Instantiate(man4, new Vector3(0, 0, 90), Quaternion.identity);
+            else if (manNumber == 5)
+            {
+                m = Instantiate(man5, new Vector3(0, 0, 90), Quaternion.identity);
+                manNumber = 0;
+            }
+                
+            body = m.transform.Find("body").gameObject;
+            Debug.Log("space key was pressed");
+            StartCoroutine(loadImage());
+            manNumber++;
+        }
+            
+    }
+
+    IEnumerator loadImage()
+    {
+        Texture2D tex;
+        WWW www;
+        tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+        www = new WWW(url);
+        yield return www;
+        www.LoadImageIntoTexture(tex);
+        //m = Instantiate(man, new Vector3(0, 0, 90), Quaternion.identity);
+        //body = m.transform.Find("body").gameObject;
+        Rect rec = new Rect(0, 0, tex.width, tex.height);
+        Sprite s = Sprite.Create(tex, rec, new Vector2(0, 0), 1);
+        //body.GetComponent<Image>().sprite = s;
+    }
+}
---
+++ Assets/script/move.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+    private float x;
+    private float y;
+    private float minusX;
+    private float minusY;
+
+    // Use this for initialization
+    void Start () {
+        x = Random.Range(0.2f, 0.4f);
+        y = Random.Range(0.05f, 0.2f);
+        //Debug.Log(x);
+        //Debug.Log(y);
+        minusX = Random.value;
+        minusY = Random.value;
+        if (minusX < 0.5f)
+        {
+            //Debug.Log("a");
+            x = -x;
+        }
+        if (minusY < 0.5f)
+        {
+            //Debug.Log("b");
+            y = -y;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.position += new Vector3(x, y, 0);
+        if (transform.position.x > 215.84 || transform.position.x < -215.84)
+        {
+            //Debug.Log("c");
+            x = -x;
+        }
+        if (transform.position.y > 43.64 || transform.position.y < -43.64)
+        {
+            //Debug.Log("d");
+            y = -y;
+        }
+
+        //Debug.Log(x);
+        //Debug.Log(y);    
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+        x = -x;
+        y = -y;
+    }
+}

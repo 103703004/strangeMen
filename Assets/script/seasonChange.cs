3bdfa2c17d6a1fe23813c809ed8cb17430e5b9e7
Wan-Ting Huang <103703004@nccu.edu.tw>
2018-03-07 14:50:47 +0800

    changeSeason

--- Assets/script/seasonChange.cs
+++ Assets/script/seasonChange.cs
@@ -5,14 +5,28 @@ using UnityEngine;
 public class seasonChange : MonoBehaviour {
 
     public static string season;
+    private float time = 10;
 
 	// Use this for initialization
 	void Start () {
         season = "spring";//spring;summer;fall;winter
-	}
+        InvokeRepeating("changeSeason", time, time);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        
+    }
+
+    private void changeSeason()
+    {
+        if (season.Equals("spring"))
+            season = "summer";
+        else if (season.Equals("summer"))
+            season = "fall";
+        else if (season.Equals("fall"))
+            season = "winter";
+        else if (season.Equals("winter"))
+            season = "spring";
+    }
 }

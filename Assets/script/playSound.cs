50bdb7bf2aba57385832eaf34bdd35fee159023d
Wan-Ting Huang <103703004@nccu.edu.tw>
2018-05-07 12:56:38 +0800

    add sound

---
+++ Assets/script/playSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSound : MonoBehaviour {
+
+    public AudioClip tree;
+    public AudioSource audioS;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void playTree()
+    {
+        audioS.PlayOneShot(tree);
+    }
+}

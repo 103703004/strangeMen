ed8b2d8213d0a331baec46d1ab5b5f8f560f68dc
Wan-Ting Huang <103703004@nccu.edu.tw>
2018-05-12 20:54:39 +0800

    秋 sound

--- Assets/script/playSound.cs
+++ Assets/script/playSound.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 public class playSound : MonoBehaviour {
 
     public AudioClip tree;
-    public AudioSource audioS;
+    public AudioClip moonFest;
+    public AudioClip maple;
+    public AudioClip crab;
+    private AudioSource audioS;
 
 	// Use this for initialization
 	void Start () {
-		
+        audioS = GameObject.FindGameObjectWithTag("audio").GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void playMaple()
+    {
+        audioS.PlayOneShot(maple);
+    }
+
+    void playCrab()
+    {
+        audioS.PlayOneShot(crab);
+    }
+
+    void playMoonFest()
+    {
+        audioS.PlayOneShot(moonFest);
+    }
 
     void playTree()
     {

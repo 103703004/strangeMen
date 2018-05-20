3842876c2b4d32b07b8f3fc60ffd7150e455e43f
Wan-Ting Huang <103703004@nccu.edu.tw>
2018-05-20 18:37:30 +0800

    季節變換 menu消失

--- Assets/script/seasonChange.cs
+++ Assets/script/seasonChange.cs
@@ -6,13 +6,17 @@ public class seasonChange : MonoBehaviour {
 
     private Animation anim;
     public static string season;
-    private float time = 30;
+    private float time = 10;
+    private GameObject[] menus;
+    private Animation anim1;
+    private string manNumber;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         anim = gameObject.GetComponent<Animation>();
         season = "spring";//spring;summer;fall;winter
         InvokeRepeating("changeSeason", time, time);
+        
     }
 	
 	// Update is called once per frame
@@ -42,5 +46,45 @@ public class seasonChange : MonoBehaviour {
             season = "spring";
             anim.Play("WinterToSpring");
         }
+
+        menus = GameObject.FindGameObjectsWithTag("menu");
+        foreach (GameObject menu in menus)
+        {
+            if (menu.activeSelf)
+            {
+                anim1 = menu.transform.parent.GetComponent<Animation>();
+                manNumber = menu.transform.parent.tag;
+                if (manNumber.Equals("man1"))
+                {
+                    anim1["man1"].wrapMode = WrapMode.Loop;
+                    anim1.PlayQueued("man1");
+                }
+                else if (manNumber.Equals("man2"))
+                {
+                    anim1["man2"].wrapMode = WrapMode.Loop;
+                    anim1.PlayQueued("man2");
+                }
+                else if (manNumber.Equals("man3"))
+                {
+                    anim1["man3"].wrapMode = WrapMode.Loop;
+                    anim1.PlayQueued("man3");
+                }
+                else if (manNumber.Equals("man4"))
+                {
+                    anim1["man4"].wrapMode = WrapMode.Loop;
+                    anim1.PlayQueued("man4");
+                }
+                else if (manNumber.Equals("man5"))
+                {
+                    anim1["man5"].wrapMode = WrapMode.Loop;
+                    anim1.PlayQueued("man5");
+                }
+                menu.transform.parent.gameObject.GetComponent<move>().isMove = true;
+                menu.transform.parent.GetComponent<press>().isPress = false;
+                menu.SetActive(false);
+            }
+        }
+
     }
+
 }
